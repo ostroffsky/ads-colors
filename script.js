f44de98c961de58be780d1198f9e93b852579343
Alexey Ostrovsky <alexey.ostrovsky@vkteam.ru>
2025-02-28 17:39:43 +0300

    add fader

--- script.js
+++ script.js
@@ -1,52 +1,110 @@
-function calculateColors() {
-    const colorThief = new ColorThief();
+const colorThief = new ColorThief();
 
+function buildTable() {
+/*    <tr className="item">
+        <td className="item-img">
+            <div className="item-img-w"><img src="1.jpg" alt="1"/></div>
+        </td>
+        <td className="item-img">
+            <div className="item-img-w"><img src="1.jpg" alt="1"/></div>
+        </td>
+        <td className="item-img">
+            <div className="item-img-w"><img src="1.jpg" alt="1"/></div>
+        </td>
+        <td className="item-result"></td>
+        <td className="item-accent"></td>
+    </tr>
+
+   */
+
+    var table = document.querySelector("table");
+    for (var i = 1; i <= 13; i++) {
+        table.appendChild(createTr(i));
+    }
+}
+
+function createTr(num) {
+    var tr = document.createElement("tr");
+    tr.className = "item";
+
+    tr.appendChild(createTd(num));
+    tr.appendChild(createTd(num));
+    tr.appendChild(createTd(num));
+
+    var tdResult = document.createElement("td");
+    tdResult.className = "item-result";
+    tr.appendChild(tdResult);
+
+    var tdAccent = document.createElement("td");
+    tdAccent.className = "item-accent";
+    tr.appendChild(tdAccent);
+
+    return tr;
+}
+
+function createTd(num) {
+    var td = document.createElement("td");
+    td.className = "item-img";
+
+    var div = document.createElement("div");
+    div.className = "item-img-w";
+
+    var faderDiv = document.createElement("div");
+    faderDiv.className = "item-img-fader";
+
+    var img = document.createElement("img");
+    img.src = num + ".jpg";
+
+    faderDiv.appendChild(img);
+    div.appendChild(faderDiv);
+    td.appendChild(div);
+
+    return td;
+}
+
+function calculateColors() {
     const items = document.querySelectorAll('.item');
     items.forEach(value => {
-        console.log(value);
-
         const img = value.querySelector('img');
-        console.log(img);
-
-        var color = [0, 0, 0];
-        var palette = [];
 
         // Make sure image is finished loading
         if (img.complete) {
-            color = colorThief.getColor(img);
-            palette = colorThief.getPalette(img, 10);
+            console.log("img was complete");
+            calculateColor(value, img);
         } else {
             img.addEventListener('load', function () {
-                color = colorThief.getColor(img);
-                palette = colorThief.getPalette(img, 10);
-            });
-            img.addEventListener('error', function () {
-                alert('error')
+                console.log("img inited via listener");
+                calculateColor(value, img);
             });
         }
 
-        console.log(color);
-        console.log(palette);
-
-        value.querySelector(".item-result").setAttribute("style", "background: " + getRgbFromArray(color) + ";");
-        value.querySelector(".item-result").appendChild(createColor(color, "Средний цвет"));
-
-        palette.sort((a, b) => calculateSaturation(a[0], a[1], a[2]) - calculateSaturation(b[0], b[1], b[2]));
-
-        palette.forEach(c => {
-            value.querySelector(".item-result").appendChild(createColor(c));
-        });
-
-        let accent = findAccentColor(palette);
-        value.querySelector(".item-accent").appendChild(createColor(accent, "Акцент"));
-        var mostSaturated = palette[palette.length - 1];
-        value.querySelector(".item-accent").appendChild(createColor(mostSaturated, "Насыщенный"));
-        //value.querySelector(".item-accent").appendChild(createColor(increaseContrast(accent, 0.8)));
-
-        value.querySelector(".item-img-w").setAttribute("style", "background: " + getRgbFromArray(color) + ";");
-        value.querySelector(".item-img:nth-child(2) .item-img-w").setAttribute("style", "background: " + getRgbFromArray(accent) + ";");
-        value.querySelector(".item-img:nth-child(3) .item-img-w").setAttribute("style", "background: " + getRgbFromArray(mostSaturated) + ";");
     });
+}
+
+function calculateColor(tr, img) {
+    var color = [0, 0, 0];
+    var palette = [];
+
+    color = colorThief.getColor(img);
+    palette = colorThief.getPalette(img, 10);
+
+    tr.querySelector(".item-result").setAttribute("style", "background: " + getRgbFromArray(color) + ";");
+    tr.querySelector(".item-result").appendChild(createColor(color, "Средний цвет"));
+
+    palette.sort((a, b) => calculateSaturation(a[0], a[1], a[2]) - calculateSaturation(b[0], b[1], b[2]));
+
+    palette.forEach(c => {
+        tr.querySelector(".item-result").appendChild(createColor(c));
+    });
+
+    let accent = findAccentColor(palette);
+    tr.querySelector(".item-accent").appendChild(createColor(accent, "Акцент"));
+    var mostSaturated = palette[palette.length - 1];
+    tr.querySelector(".item-accent").appendChild(createColor(mostSaturated, "Насыщенный"));
+
+    tr.querySelector(".item-img-w").setAttribute("style", "background: " + getRgbFromArray(color) + ";");
+    tr.querySelector(".item-img:nth-child(2) .item-img-w").setAttribute("style", "background: " + getRgbFromArray(accent) + ";");
+    tr.querySelector(".item-img:nth-child(3) .item-img-w").setAttribute("style", "background: " + getRgbFromArray(mostSaturated) + ";");
 }
 
 function getRgb(r, g, b) {
@@ -54,6 +112,7 @@ function getRgb(r, g, b) {
 }
 
 function getRgbFromArray(rgbArr) {
+    rgbArr = rgbArr || [0,0,0];
     return getRgb(rgbArr[0], rgbArr[1], rgbArr[2]);
 }
 
@@ -96,6 +155,8 @@ function rgbToHex(rgbArr) {
 }
 
 function createColor(rgbArr, text) {
+    rgbArr = rgbArr || [0, 0, 0];
+
     var div = document.createElement("div");
     div.setAttribute("class", "color");
     div.setAttribute("style", "background: " + getRgbFromArray(rgbArr) + ";");
@@ -161,6 +222,7 @@ function findAccentColor(colors) {
 }
 
 window.onload = function () {
+    buildTable();
     calculateColors();
 };
 
